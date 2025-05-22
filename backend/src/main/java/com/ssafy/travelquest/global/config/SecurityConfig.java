4f6f4552ed8e1f25de7029ca6b9a351aca04542a
dign552 <dign552@naver.com>
2025-05-22 17:57:35 +0900

    feat: security

--- backend/src/main/java/com/ssafy/travelquest/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/travelquest/global/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
-                        		"/api/v1/**", // 테스트용 잠깐만
+//                        		"/api/v1/**", // 테스트용 잠깐만
                                 "/api/v1/oauth/**",
                                 "/auth/**",            // 로그인, 토큰 리프레시
                                 "/oauth2/**",          // OAuth 콜백
